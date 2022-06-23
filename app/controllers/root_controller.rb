606f66e9f556c4659d700dd7e04511e3c45657bd
Rohit Kumar <rohit952513@gmail.com>
2022-06-23 12:17:20 +0530

    basic prototype

---
+++ app/controllers/root_controller.rb
@@ -0,0 +1,83 @@
+class RootController < ApplicationController
+
+    def index
+        @posts=Post.all
+        render "index"
+    end
+
+    def about
+        # puts "------------------------------------"
+        # puts "about in server"
+        render "about"
+    end
+
+    def contact
+        # puts "------------------------------------"
+        # puts "contact in server"
+        # redirect_to('/about')
+        render "contact"
+    end
+
+    def api
+        @response={      #hash map - key value pair
+            :status => 200,
+            :message => "hello world"
+        }
+        render json: @response
+    end
+
+    def user
+        render json: params
+        # render json: params[:name]
+    end
+
+    def add
+        # render json:params
+        @post=Post.new
+        @post.title=params[:title]
+        @post.description=params[:description]
+        @post.save
+        # @response={     
+        #     :status => 200,
+        #     :message => "Saved Successfully"
+        # }
+        # render json: @response
+        @posts=Post.all
+        render "index"
+        
+    end
+
+    def delete
+        @post=Post.find(params[:id])
+        @post.destroy
+        @posts=Post.all
+        render "index"
+        # render JSON: params
+    end
+
+    def search
+        @query=params[:query]
+        @posts=Post.where("posts.title LIKE ?",["%#{@query}%"])
+        render "index"
+    end
+
+    def update
+        @post=Post.find(params[:id])
+        @id=@post.id
+        @title=@post.title
+        @description=@post.description
+        render "update"
+    end
+
+    def updateform
+        # render JSON:params
+        @post=Post.find(params[:id])
+        @post.title=params[:title]
+        @post.description=params[:description]
+        @post.save
+        @posts=Post.all
+        render "index"
+    end
+
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,18 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+
+  # get '/index' ,to: "root#index"
+  get '/about' ,to: "root#about"
+  get '/contact' ,to: "root#contact"
+  get 'response' ,to: "root#api"
+  get 'user/:id/:name' ,to: "root#user"
+  post '/add' ,to: "root#add"
+  get '/delete/:id' ,to: "root#delete"
+  get '/search' ,to: "root#search"
+  get '/update/:id' ,to: "root#update"
+  post '/updateform' ,to: "root#updateform"
+  root "root#index"
+end
