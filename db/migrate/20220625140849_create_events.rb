8c33d1bcf894659b1f69ac7404bcf0e315759728
Rohit Kumar <rohit952513@gmail.com>
2022-06-26 16:33:55 +0530

    Added postgre

--- db/migrate/20220625140849_create_events.rb
+++ db/migrate/20220625140849_create_events.rb
@@ -1,15 +1,15 @@
 class CreateEvents < ActiveRecord::Migration[7.0]
   def change
     create_table :events do |t|
-      t.string :title
+      t.text :title
       t.text :description
       t.text :cover_image
       t.datetime :Reg_ST
       t.datetime :Reg_ET
       t.integer :fees
       t.text :venue
-      t.string :event_type
-      t.string :tag
+      t.text :event_type
+      t.text :tag
 
       t.timestamps
     end
